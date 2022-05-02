f70bd5e00e8024ba96dbf408eb18c706b0b89a4f
Zmrbak <20995658@qq.com>
2022-05-02 17:37:14 +0800

    add code

---
+++ 配套代码/ida03/ida03/ida03.cpp
@@ -0,0 +1,33 @@
+﻿// ida03.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
+//
+
+#include <stdio.h>
+
+//int fun_1(int i);
+//int fun_2(int i);
+//int fun_3(int i);
+
+int fun_3(int i)
+{
+    printf("This is Func 1 %d\n" ,(i + 3));
+    return i;
+}
+
+int fun_2(int i)
+{
+    printf("This is Func 1 %d\n", (i + 2));
+    return  fun_3(i);
+}
+
+int fun_1(int i) 
+{
+    printf("This is Func 1 %d\n", (i + 1));
+    return  fun_2(i);
+}
+
+
+int main()
+{
+    printf("Hello World!\n");
+    fun_1(1);
+}
